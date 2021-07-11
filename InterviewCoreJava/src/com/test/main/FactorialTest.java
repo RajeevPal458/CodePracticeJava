ab9fa3064e4672b1b41c8f02dbe04c47f402408a
RajeevPal458 <58217830+RajeevPal458@users.noreply.github.com>
2021-07-10 21:31:15 -0700

    commit for SAM-2
    commit for SAM-2

--- InterviewCoreJava/src/com/test/main/FactorialTest.java
+++ InterviewCoreJava/src/com/test/main/FactorialTest.java
@@ -5,12 +5,12 @@ public class FactorialTest {
 	public static void main(String[] args) {
 		int fact = getFactorial(5);
 		System.out.println("factorial of 5 is:"+fact);
+		System.out.println("factorial of 5 is:"+fact);
 		
 	}
 	
 	public int getFactorial(int num){
 		if(num <=1) return 1;
-		
 		return num*getFactorial(num-1);
 	}
 }
