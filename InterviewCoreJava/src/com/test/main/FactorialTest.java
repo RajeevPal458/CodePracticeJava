891ffde88faea635b000a449e33a65bb4894713a
RajeevPal458 <58217830+RajeevPal458@users.noreply.github.com>
2021-07-10 21:27:40 -0700

    Update FactorialTest.java
    added factorial code logic

--- InterviewCoreJava/src/com/test/main/FactorialTest.java
+++ InterviewCoreJava/src/com/test/main/FactorialTest.java
@@ -3,6 +3,14 @@ package com.test.main;
 public class FactorialTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int fact = getFactorial(5);
+		System.out.println("factorial of 5 is:"+fact);
+		
+	}
+	
+	public int getFactorial(int num){
+		if(num <=1) return 1;
+		
+		return num*getFactorial(num-1);
 	}
 }
